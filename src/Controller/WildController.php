6be34447be7bedeb118c06b6b46628a68269d80b
Anais Francois <anais44.francois@gmail.com>
2019-11-27 21:53:08 +0100

    fresh start

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,45 +1,37 @@
 <?php
 // src/Controller/WildController.php
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route ("/", name="wild_")
+ * @package App\Controller
+ */
 Class WildController extends AbstractController
 {
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index(): Response
+    public function index() : Response
     {
         return $this->render('wild/index.html.twig', [
-            'website' => 'wild Séries',
+            'website' => 'Wild Séries',
         ]);
     }
-
     /**
-     * @Route("/wild/show/{slug<^[a-z0-9-]+$>}",
-     *     defaults={"slug"= null},
-     *     name="wild_show"
-     * )
-     * @return Response
+     * @Route("/wild/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="wild_show",
+     *     defaults={"slug" = "Aucune série sélectionnée, veuillez choisir une série"})
+     *
+     *
      */
-    public function show( $slug): Response
+    public function show (string $slug ) : Response
     {
-        if (!$slug) {
-            throw $this -> createNotFoundException("No slug has been sent to find a program in program\'s table .");
-        }
-        $slug = preg_replace(
-            '/-/',
-            ' ', ucwords($slug)
-        );
-
+        $slug = str_replace("-", " ", $slug);
+        $slug = ucwords($slug);
         return $this->render('wild/show.html.twig', [
             'slug' => $slug,
         ]);
-
     }
-
 }
-
